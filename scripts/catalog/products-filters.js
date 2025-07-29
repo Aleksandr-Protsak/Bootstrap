3395b0e3901b3aab6fbb9668bf3d9ac3704acc0f
Aleksandr-Protsak <45597787+Aleksandr-Protsak@users.noreply.github.com>
2025-07-29 18:50:35 +0300

    Update products-filters.js

--- scripts/catalog/products-filters.js
+++ scripts/catalog/products-filters.js
@@ -52,10 +52,11 @@ const categoryFilters = [
     const html = [];
 
     for(const filterItem of filterItems) {
+        const filterInputId = `${filterItem.name}_${filterItem.id}`
         html.push(`
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" />
-                <label class="form-check-label">${filterItem.name}</label>
+                <input type="checkbox" class="form-check-input" id="${filterInputId}" value="${filterItem.id}" name="${filterInputId}" />
+                <label class="form-check-label" for="${filterInputId}">${filterItem.name}</label>
             </div>
         `)
     }
@@ -69,11 +70,91 @@ for(const filter of filtersAttributes) {
     const checkboxesGroup = categoryFilters.filter(cf => cf.filtersAttributesId === filter.id);
 
     filtersHTML.push(`
-        <fieldset class="mb-4">
+        <fieldset class="mb-4" id="${filter.slug}">
             <h6>${filter.name}</h6>
             ${getCheckboxFilterHTML(checkboxesGroup)}
         </fieldset>
     `);
 }
 
+products.forEach((product, index) => {
+    const attributes = [];
+
+    for(let i = 0; i < 4; i++ ) {
+        const coef = (index + 1) / products.length;
+        const filterAttributeId = Math.floor(coef * 3) + 1;
+        const maxItemsLength = categoryFilters.filter((cf) => cf.filtersAttributesId === filterAttributeId).length;
+        const itemId = Math.floor(coef * maxItemsLength) + 1;
+        
+        attributes.push({ itemId, filterAttributeId });
+    }
+
+    product.attributes = attributes;
+});
+
 filtersFormEl.insertAdjacentHTML('afterbegin', filtersHTML.join(''));
+
+const filterProducts = (appliedFilters) => {
+    let results = [...products];
+
+    if(appliedFilters?.length) {
+        results = results.filter(
+            product => product.attributes.some(
+                pa => appliedFilters.some(af => pa.itemId === af.itemId && pa.filterAttributeId === af.attributeId )
+            )
+        );
+    }
+    const productsListChanged = new CustomEvent('productsListChanged', { detail: { products: results, origin: 'filters' } });
+    document.dispatchEvent(productsListChanged);
+    updateQueryParam('filters', JSON.stringify(appliedFilters));
+    updateQueryParam('page', 1);
+}
+
+const applyFiltersBtnEl = document.getElementById('applyFiltersBtn');
+
+applyFiltersBtnEl.addEventListener('click', () => {
+    const appliedFilters = [];
+
+    for(const filter of filtersAttributes) {
+        const fieldsetEl = document.getElementById(filter.slug);
+        const inputs = fieldsetEl.querySelectorAll('input');
+        
+        inputs.forEach((input) => {
+            if(input.checked) {
+                appliedFilters.push({ itemId: +input.value, attributeId: filter.id  });
+            }
+        });
+    }
+
+    filterProducts(appliedFilters);
+});
+
+const clearFiltersBtnEl = document.getElementById('clearFiltersBtn');
+
+clearFiltersBtnEl.addEventListener('click', () => {
+    for(const filter of filtersAttributes) {
+        const fieldsetEl = document.getElementById(filter.slug);
+        const inputs = fieldsetEl.querySelectorAll('input');
+        
+        inputs.forEach((input) => {
+            input.checked = false;
+        });
+    }
+
+    filterProducts([]);
+});
+
+const params = getQueryParam();
+const filtersObj = params.filters ? JSON.parse(params.filters) : [];
+
+for(const filter of filtersAttributes) {
+    const fieldsetEl = document.getElementById(filter.slug);
+    const inputs = fieldsetEl.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+
+        input.checked = filtersObj.some(fo => fo.itemId === +input.value && fo.attributeId === +filter.id);
+    });
+}
+
+filterProducts(filtersObj);
