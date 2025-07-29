4f5fd5b738e884adc15824a00bddb4ce2242b667
Aleksandr-Protsak <45597787+Aleksandr-Protsak@users.noreply.github.com>
2025-07-29 18:50:03 +0300

    Update events-handlers.js

--- scripts/catalog/events-handlers.js
+++ scripts/catalog/events-handlers.js
@@ -1,19 +1,32 @@
-const cardElements = document.getElementsByClassName(cardElClass);
-const showProductDetailEventName = 'showProductDetail';
+let cardElementsListener = [];
 
-for(const cardElement of cardElements) {
-    cardElement.addEventListener('click', (event) => {
-        const targetElement = event.target;
-        const parentNode = targetElement.closest('[data-app-product-id]');
-        const productId = Number(parentNode.dataset.appProductId);
+updateCardListeners = () => {
+    const cardElements = document.getElementsByClassName(cardElClass);
 
-        if(!productId) throw Error('Product id is undefined');
+    if(cardElementsListener.length) {
+        cardElementsListener.forEach(element => {
+            element.removeEventListener('click', element);
+        });
+        cardElementsListener = [];
+    }
 
-        const targetProductData = products.find(p => p.id === productId);
+    for(const cardElement of cardElements) {
+        cardElementsListener.push(cardElement);
 
-        if(!targetProductData) throw Error('Product data is not found');
+        cardElement.addEventListener('click', (event) => {
+            const targetElement = event.target;
+            const parentNode = targetElement.closest('[data-app-product-id]');
+            const productId = Number(parentNode.dataset.appProductId);
 
-        const showProductDetailEvent = new CustomEvent(showProductDetailEventName, { detail: targetProductData });
-        document.dispatchEvent(showProductDetailEvent);
-    });
+            if(!productId) throw Error('Product id is undefined');
+
+            const targetProductData = products.find(p => p.id === productId);
+
+            if(!targetProductData) throw Error('Product data is not found');
+
+            const showProductDetailEvent = new CustomEvent(showProductDetailEventName, { detail: targetProductData });
+
+            document.dispatchEvent(showProductDetailEvent);
+        });
+    }
 }
